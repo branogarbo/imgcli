d563889500ece0d6340cb2388b725ed9462fbfb5
Brian Longmore <branodev@gmail.com>
2021-10-09 00:00:02 -1000

    change email

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2021 Brian Longmore brianl.ext@gmail.com
+Copyright © 2021 Brian Longmore branodev@gmail.com
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
